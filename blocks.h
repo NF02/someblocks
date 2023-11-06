5303625273b92b9522ad8668065f56cf8c62834f
Nicola Ferru Aka NFVblog <ask.nfvblog@outlook.it>
2023-11-06 16:13:59 +0100

    my config

--- blocks.h
+++ blocks.h
@@ -1,27 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"󰍛", "$HOME/.config/scripts/bar/mem.sh",	2,		0},
-
-	{"", "$HOME/.config/scripts/bar/swap.sh",	2,		0},
-
-    {"", "$HOME/.config/scripts/bar/cpu.sh",  2, 0},
-
-    {"", "$HOME/.config/scripts/bar/cputemp.sh",    2,      0},
-
-    {"", "$HOME/.config/scripts/bar/screentemp-gammastep.sh",  15,     0},
-
-    {"", "$HOME/.config/scripts/bar/network-traffic.sh",  5,      0},
-
-    {"", "$HOME/.config/scripts/bar/updates.sh", 60, 0},
-
-    {" ", "$HOME/.config/scripts/bar/uptime.sh", 60, 0},
-
-    {" ", "cat /tmp/keyboard_layout",                                  1,      0},
-
-	{"", "$HOME/.config/scripts/bar/date.sh",		10,		0},
+  {"", "$HOME/.local/bin/statusbar/sb-music",     4, 0},
+  {"", "$HOME/.local/bin/statusbar/sb-battery",   15, 0},
+  {"", "$HOME/.local/bin/statusbar/sb-cpubars",   2,  0},
+  {"", "$HOME/.local/bin/statusbar/sb-memory",	  2,  0},
+  {"", "$HOME/.local/bin/statusbar/sb-internet", 14,  0},
+  {"", "$HOME/.local/bin/statusbar/sb-volume",    1,   0 },
+  {"", "$HOME/.local/bin/statusbar/sb-clock",     1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
